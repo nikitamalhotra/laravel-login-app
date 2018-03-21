d81195c3fd5f71292649d7fc67c6e70aa6acb2c9
Nikita Malhotra <nikita.malhotras@gmail.com>
2018-03-21 22:59:39 +0530

    Section 23

--- app/User.php
+++ app/User.php
@@ -5,25 +5,32 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * @property mixed role
+ */
 class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role_id'
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+	public function role(){
+		return $this->belongsTo('App\Role');
+	}
+
+	public function isAdmin(){
+		if($this->role->name == 'Administrator'){
+			return true;
+		}
+		return false;
+	}
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	$user = Auth::user();
+	if($user->isAdmin()){
+		echo "Admin User";
+	}
+    //return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('admin/user/role', ['middleware' => ['role', 'auth', 'web'], function() {}]);  //For Multiple Middleware
+Route::get('admin/user/role', ['middleware' => 'role', function() {
+	return "Middleware Role";
+}]);
